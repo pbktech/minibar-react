75937dd640e289c6735950d2765b0bc61a787d26
David Arbitman <david@arbitman.net>
2020-09-19 18:44:09 -0500

    added createAction and createReducer

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,26 +1,24 @@
 import { ADD_TO_CART, REMOVE_FROM_CART } from '../actions/actions';
+import { createAction, createReducer } from '@reduxjs/toolkit'
 
 const initialState = {
   cart: [],
 };
+const addToCart = createAction(ADD_TO_CART)
+const removeFromCart = createAction(REMOVE_FROM_CART);
+const incrementByAmount = createAction('counter/incrementByAmount')
 
-function rootReducer(state = initialState, action) {
-  switch (action.type) {
-    case ADD_TO_CART:
-      return {
-        cart: [
+const rootReducer = createReducer(initialState, (builder) => {
+  builder
+      .addCase(addToCart, (state, action) => {
+        state.cart = [
           ...state.cart,
           action.item,
-        ],
-      };
-    case REMOVE_FROM_CART:
-      return {
-        cart: state.cart.filter((item, index) => index != action.id),
-      };
-
-    default:
-      return state;
-  }
-}
+        ]
+      })
+      .addCase(removeFromCart, (state, action) => {
+        state.cart = state.cart.filter((item, index) => index != action.id)
+      })
+});
 
 export default rootReducer;

a473ef46a19b0a73e75f11f99bef0e9979340449
David Arbitman <david@arbitman.net>
2020-09-26 14:54:53 -0500

    removed unnecessary stuff

--- src/utils.js
+++ src/utils.js
@@ -2,27 +2,6 @@ import React from "react";
 
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-export const dateSort = (a, b) => {
-  let sort_field = "end_date";
-  if (typeof a.degree_date !== "undefined") {
-    sort_field = "degree_date";
-  }
-
-  const [dateAMonth, dateAYear] = a[sort_field].split('/');
-  const dateA = new Date(dateAYear, dateAMonth - 1, 1);
-  const [dateBMonth, dateBYear] = b[sort_field].split('/');
-  const dateB = new Date(dateBYear, dateBMonth - 1, 1);
-
-  if (dateAMonth.toLowerCase() === "present") {
-    return 1;
-  }
-  if (dateBMonth.toLowerCase() === "present") {
-    return -1;
-  }
-
-  return dateA - dateB;
-}
-
 export const displayDate = (d) => {
   if (d.toLowerCase() === "present") {
     return d;
